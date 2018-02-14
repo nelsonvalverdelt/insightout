350c90a91cc373476a493544dad97484114fbf52
Nelson Valverde La Torre <nelsonvalverdelt@outlook.com>
2018-02-14 17:20:20 -0500

    plugins, snackbar style and notification messages

---
+++ js/service.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }    
+
+
+$("#send").on("click", function(){
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    if($.trim(name).length> 0 && $.trim(email).length > 0)
+    {
+        if(validateEmail(email))
+        {
+            $.ajax({
+                url: "service.php",
+                type: "POST",
+                data: { name: name, email: email, message: message},
+                success: function(resp){
+        
+                        if(resp){
+                            Clear();
+                            $("#snackbar").text("¡Muchas gracias por tu interés!");
+                            var x = document.getElementById("snackbar");
+                            x.className = "show";
+        
+                            setTimeout(function(){
+                                 x.className = x.className.replace("show", ""); 
+                                }, 3000);
+                        }else{
+                            alert("Error en el servidor");
+                        }
+                },
+                error: function(error){
+                    alert("Error en el servicio :(");
+                }
+                
+                });
+        }
+        
+    }else{
+        $("#snackbar-error").text("¡Completar los campos vacios, por favor!");
+        var x = document.getElementById("snackbar-error");
+        x.className = "show";
+
+        setTimeout(function(){
+             x.className = x.className.replace("show", ""); 
+            }, 3000);
+    }
+
+});
+
+
+
+$("#send2").on("click",function(event){
+    var email = $("#email").val();
+
+    if($.trim(email).length > 0)
+    {
+        if(validateEmail(email))
+        {
+            $.ajax({
+                url: "service.php",
+                type: "POST",
+                data: {email: email},
+                success: function(resp){
+        
+                        if(resp){
+                            //alert("Su reserva fue realizada correctamente");
+                            $("#snackbar").text("¡Muchas gracias por tu interés!");
+                            Clear2();
+                            var x = document.getElementById("snackbar");
+                            x.className = "show";
+        
+                            setTimeout(function(){
+                                 x.className = x.className.replace("show", ""); 
+                                 //window.location.href = "../";                          
+                                }, 3000);
+                                
+                            event.preventDefault();
+                        }else{
+                            alert("Error en el servidor");
+                        }
+                },
+                error: function(error){
+                    alert("Error en el servicio :(");
+                }
+                
+                });
+
+        }
+
+    }else{
+        $("#snackbar-error").text("¡Completar los campos vacios, por favor!");
+        var x = document.getElementById("snackbar-error");
+        x.className = "show";
+
+        setTimeout(function(){
+             x.className = x.className.replace("show", ""); 
+            }, 3000);
+    }
+});
+
+
+function Clear()
+{
+    $("#name").val('');
+    $("#email").val('');
+    $("#message").val('');
+}
+function Clear2()
+{
+    $("#email").val('');
+}
+
+});
---
+++ service.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_set('America/Lima');
+$fecha = date('m-d-Y');
+$tiempo = date('H:i:s');
+if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) ){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = nl2br($_POST['message']);
+    $serverName = "io-server.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "InsightOut",
+        "Uid" => "io",
+        "PWD" => "@Developer19"
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+ // Set up the proc params array - be sure to pass the param by reference
+$params = array(
+    array(&$name, SQLSRV_PARAM_OUT),
+    array(&$email, SQLSRV_PARAM_OUT),
+    array(&$message, SQLSRV_PARAM_OUT)
+    );
+
+    if($conn)
+    {
+        $sql = "EXEC INSERT_CONTACT_SP @NAME = ?, @EMAIL = ?, @MESSAGE = ?";
+        
+        $statement = sqlsrv_prepare($conn, $sql, $params);   
+        if($statement){
+            echo json_encode(TRUE);
+        }else{
+            echo json_encode("Internal Server");
+        }
+    }else{
+        echo json_encode("Internal Server");
+    }
+  
+}
